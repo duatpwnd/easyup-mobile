994ef2fd00e1bb7d8d3c81010cd4d8e0aaa9a1b8
duatpwnd <duatpwnd1@naver.com>
2021-01-11 12:56:41 +0900

    typescript apply

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,40 @@ module.exports = {
         "@": path.join(__dirname, "src/"),
       },
     },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+          options: {
+            loaders: {
+              scss: "vue-style-loader!css-loader!sass-loader",
+              sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax",
+            },
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]?[hash]",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".vue", ".json"],
+      alias: {
+        vue$: "vue/dist/vue.esm.js",
+      },
+    },
   },
 };
