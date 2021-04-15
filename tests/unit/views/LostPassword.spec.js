96abd65116196e136bd781c4360c2ff5b520cc0c
duatpwnd <duatpwnd1@naver.com>
2021-04-15 17:54:49 +0900

    이메일로 비밀번호 찾기 단위테스트 작성

--- tests/unit/views/LostPassword.spec.js
+++ tests/unit/views/LostPassword.spec.js
@@ -1,7 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import LostPassword from "@/views/LostPassword.vue";
 import Vue from "vue";
-import VueRouter from "vue-router";
 import axios from "axios";
 import ApiUrl from "@/assets/js/api_url.js";
 import GlobalUrl from "@/plugin/global_plugin.js";
@@ -10,16 +9,17 @@ describe("LostPassword.vue", () => {
   // 이메일 정규식
   const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
   beforeEach(() => {
-    Vue.use(VueRouter);
-    Vue.prototype.$axios = axios;
-    Vue.prototype.$ApiUrl = ApiUrl;
-    Vue.prototype.$GlobalUrl = GlobalUrl;
-    const router = new VueRouter({
-      mode: "history",
-      base: process.env.BASE_URL,
-      duplicateNavigationPolicy: "ignore",
+    wrapper = shallowMount(LostPassword, {
+      mocks: {
+        $EventBus: new Vue(),
+        $axios: axios,
+        $ApiUrl: ApiUrl,
+        $GlobalUrl: GlobalUrl,
+      },
     });
-    wrapper = shallowMount(LostPassword, { router });
+  });
+  test("이메일 주소를 미입력할 경우", () => {
+    expect(wrapper.vm.email.length).toBe(0);
   });
   test("이메일 주소를 입력할 경우", () => {
     wrapper.vm.email = "duatpwnd1@naver.com";
